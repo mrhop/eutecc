ea8e47660fb279443885d23dbf6bead6e13c3fea
mrhop <233402923@qq.com>
2017-11-02 16:36:13 +0800

    updating

--- public/js/dev/self/basic.js
+++ public/js/dev/self/basic.js
@@ -8,9 +8,19 @@ var utilFun = require("utilFun");
 $(document).ready(function () {
     //onclick give active
     $(".navbar-nav li.active").removeClass("active");
-    $(".navbar-nav li."+$("body").attr("id")+"-li").addClass("active");
-    $(".navbar-nav li."+$("body").attr("id")+"-li-parent").addClass("active");
-    $(".left-menu li."+$("body").attr("id")).addClass("active");
+    $(".navbar-nav li." + $("body").attr("id") + "-li").addClass("active");
+    $(".navbar-nav li." + $("body").attr("id") + "-li-parent").addClass("active");
+    $(".left-menu li." + $("body").attr("id")).addClass("active");
+
+    $(".top-logo .top-logo-img").click(function () {
+        if ($(window).width() <= 768) {
+            if ($(".navbar.navbar-default").is(":hidden")) {
+                $(".navbar.navbar-default").slideDown()
+            } else {
+                $(".navbar.navbar-default").slideUp()
+            }
+        }
+    })
     if ($("body").attr("id") === 'index') {
         $('.first-container .flexslider').flexslider({
             fadeFirstSlide: false,
