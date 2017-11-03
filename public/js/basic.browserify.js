078fecc01249aed66037c32a6760dfda755ce80f
mrhop <233402923@qq.com>
2017-11-03 14:25:32 +0800

    updating

--- public/js/basic.browserify.js
+++ public/js/basic.browserify.js
@@ -55,6 +55,18 @@ $(document).ready(function () {
             nextText: "",
         });
     }
+
+    if (($(window).height() + 100) < $(document).height()) {
+        $('#top-link-block').removeClass('hidden').affix({
+            // how far to scroll down before link "slides" into view
+            offset: {top: 100}
+        });
+    }
+
+    $("#top-link-block a").click(function () {
+        $('html,body').animate({scrollTop: 0}, 'slow');
+        return false;
+    })
 });
 
 
